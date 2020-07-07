a30bc8c1515c2d7807577e57468851e9c14bf2e4
szopiks@yahoo.com <szopiks@yahoo.com>
2020-07-06 22:43:31 -0700

    adding new files

--- src/main.js
+++ src/main.js
@@ -1,27 +1,19 @@
-import { Player } from './../src/player.js';
-import { Monster } from './../src/monsters.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import {roulette} from './roulette';
 import $ from 'jquery';
 
-let monsters = [];
-let gremlin = new Monster("Gremlin", 7, 10, 1, 2, 1);
-let newGame = new Game();
-$(document).ready(function() {
-    $('#player-input').submit(function(event) {
-        event.preventDefault();
-        let name = $('#name').val();
-        let characterClass = $('#characterClass').val();
-        let player = new Player(name, characterClass);
-        player.assignCharacterClass();
-        console.log(player);
-        $('#player-input').hide();
-        $('#player').show();
-        $("#player-name").text(name);
-        $('#player-class').text(characterClass);
-        $("#player-health").text(player.health);
-        $("#player-pizza-health").text(player.pizzaHealth);
+$(document).ready(function () {
+    var win = Math.ceil(Math.random() * 6);
+    $('#win').text(win);
+
+    $('#roulette-form').submit(function (event) {
+        event.preventDefault();  
+        $('#solution').append("<li>" + "triggers the gun" + "</li>");
+        // var output = roulette(goal);
+        // output.forEach(function (element) {
+        //     $('#solution').append("<li>" + element + "</li>");
+        // });
     });
 });
-

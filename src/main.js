80bc2c68bc9a528f99ebd96a567ac93fa6c50bef
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-07-07 16:38:53 -0700

    hardcode monster stats and add to array

--- src/main.js
+++ src/main.js
@@ -7,7 +7,14 @@ import $ from 'jquery';
 
 let monsters = [];
 let gremlin = new Monster("Gremlin", 7, 10, 1, 2, 1);
+let bugbear = new Monster("Bugbear", 10, 20, 2, 11, 2);
+let ogre = new Monster("Ogre", 12, 130, 4, 8, 3);
+let roadrage = new Monster("Roadrage", 15, 15, 6, 15, 4);
+
+monsters.push(gremlin,bugbear,ogre,roadrage);
+
 let newGame = new Game();
+
 $(document).ready(function() {
     $('#player-input').submit(function(event) {
         event.preventDefault();
@@ -32,7 +39,7 @@ $(document).ready(function() {
     $('#throw-pizza').submit(function(event) {
 
     });
-    
+
     $('#run-away').submit(function(event) {
 
     });
--- src/monsters.js
+++ src/monsters.js
@@ -12,31 +12,3 @@ Monster.prototype.takeDamage = function () {
     this.health -= damage;
     return this.health;
 }
-
-Monster.prototype.monsterClass = function () {
-    if (this.name === "Gremlin") {
-        this.health = 10;
-        this.ac = 7;
-        this.horsepower = 1;
-        this.charisma = 13;
-        this.level = 1;
-    } else if (this.name === "Bugbear") {
-        this.health = 20;
-        this.ac = 10;
-        this.horsepower = 2;
-        this.charisma = 11;
-        this.level = 2;
-    } else if (this.name === "Ogre") {
-        this.health = 30;
-        this.ac = 12;
-        this.horsepower = 4;
-        this.charisma = 8;
-        this.level = 3;
-    } else if (this.name === "Roadrage") {
-        this.health = 15;
-        this.ac = 15;
-        this.horsepower = 6;
-        this.charisma = 15;
-        this.level = 4;
-    }
-}
